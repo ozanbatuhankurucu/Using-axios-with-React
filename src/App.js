4c972ee0a5ba397cea5d2d725467de19bf773bcc
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-06-07 23:05:52 +0300

    Add custom instance

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Setup from './examples/1-first-request'
 import Setup2 from './examples/2-headers'
 import Setup3 from './examples/3-post-request'
 import Setup4 from './examples/4-global-instance'
-
+import Setup5 from './examples/5-custom-instance'
 function App() {
   return (
     <main>
@@ -11,7 +11,8 @@ function App() {
       {/* <Setup /> */}
       {/* <Setup2 /> */}
       {/* <Setup3 /> */}
-      <Setup4 />
+      {/* <Setup4 /> */}
+      <Setup5 />
     </main>
   )
 }
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,22 @@
-import { useEffect } from 'react';
-
-const randomUserUrl = 'https://randomuser.me/api';
+import { useEffect } from 'react'
+import { authFetch } from '../axios/custom'
+const randomUserUrl = 'https://randomuser.me/api'
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
-  };
+    console.log('custom axios instance')
+    try {
+      const response = await authFetch.get('/react-store-products')
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <h2 className='text-center'>custom instance</h2>;
-};
-export default CustomInstance;
+  return <h2 className='text-center'>custom instance</h2>
+}
+export default CustomInstance
