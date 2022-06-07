21549ad51a7e6faf9bb10404306c99c2d35b9a6d
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-06-07 11:58:04 +0300

    Add global instance

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Title from './components/Title'
 import Setup from './examples/1-first-request'
 import Setup2 from './examples/2-headers'
 import Setup3 from './examples/3-post-request'
+import Setup4 from './examples/4-global-instance'
+
 function App() {
   return (
     <main>
       <Title />
       {/* <Setup /> */}
       {/* <Setup2 /> */}
-      <Setup3 />
+      {/* <Setup3 /> */}
+      <Setup4 />
     </main>
   )
 }
